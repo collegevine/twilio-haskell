c3984ef244538731deeeeec0cf241aabbe614156
Ralph Morton <ralphmorton@gmail.com>
2016-05-24 20:18:47 +0200

    Basic support for adding phone numbers

--- src/Twilio/IncomingPhoneNumbers.hs
+++ src/Twilio/IncomingPhoneNumbers.hs
@@ -1,15 +1,20 @@
 {-#LANGUAGE MultiParamTypeClasses #-}
 {-#LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 module Twilio.IncomingPhoneNumbers
   ( -- * Resource
     IncomingPhoneNumbers(..)
+  , PostIncomingPhoneNumber(..)
   , Twilio.IncomingPhoneNumbers.get
+  , Twilio.IncomingPhoneNumbers.post
   ) where
 
 import Control.Applicative
 import Control.Monad.Catch
 import Data.Aeson
+import Data.Text (Text)
+import Data.Text.Encoding
 
 import Control.Monad.Twilio
 import Twilio.IncomingPhoneNumber
@@ -18,6 +23,12 @@ import Twilio.Internal.Resource as Resource
 import Twilio.Types
 
 {- Resource -}
+
+data PostIncomingPhoneNumber = PostIncomingPhoneNumber {
+    pipnPhoneNumber :: Text,
+    pipnFriendlyName :: Maybe Text,
+    pipnSmsApplicationSID :: ApplicationSID
+}
 
 data IncomingPhoneNumbers = IncomingPhoneNumbers
   { incomingPhoneNumberList :: [IncomingPhoneNumber]
@@ -31,6 +42,13 @@ instance List IncomingPhoneNumbers IncomingPhoneNumber where
 instance FromJSON IncomingPhoneNumbers where
   parseJSON = parseJSONToList
 
+instance Post1 PostIncomingPhoneNumber () where
+  post1 n = request (const $ return ()) =<<
+    makeTwilioPOSTRequest "/IncomingPhoneNumbers.json"
+      ([ ("PhoneNumber", encodeUtf8 $ pipnPhoneNumber n)
+      , ("SmsApplicationSid", encodeUtf8 . getSID $ pipnSmsApplicationSID n)
+      ] ++ maybe [] (:[]) (("FriendlyName",) . encodeUtf8 <$> pipnFriendlyName n))
+
 instance Get0 IncomingPhoneNumbers where
   get0 = request parseJSONFromResponse =<< makeTwilioRequest
     "/IncomingPhoneNumbers.json"
@@ -38,3 +56,7 @@ instance Get0 IncomingPhoneNumbers where
 -- | Get 'IncomingPhoneNumbers' for a particular country.
 get :: MonadThrow m => TwilioT m IncomingPhoneNumbers
 get = Resource.get
+
+-- | Provision a new number
+post :: MonadThrow m => PostIncomingPhoneNumber -> TwilioT m ()
+post = Resource.post
